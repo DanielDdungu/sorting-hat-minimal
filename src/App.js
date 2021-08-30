64d86bdb39be68ef18f44c3ee95bc8d74be22799
Akram <47157066+akramsoltani@users.noreply.github.com>
2021-08-30 21:06:59 +0100

    /

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { questions } from './data/Questions.js';
+
+export default function App() {
+	//Questions and house reveal setters
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showQuiz, setShowQuiz] = useState(false);
+	const [showHouse, setShowHouse] = useState(false);
+	
+	// Total score per house setters
+	const [totalGryffindor, setGryffindor] = useState(0);
+	const [totalSlytherin, setSlytherin] = useState(0);
+	const [totalRavenclaw, setRavenclaw] = useState(0);
+	const [totalHufflepuff, setHufflepuff] = useState(0);
+
+	//Final result setter
+	const [house, setHouse] = useState("Muggle");
+
+	//Sorting function
+	const answerHandler = (gryffindor, slytherin, ravenclaw, hufflepuff) => {
+		setGryffindor(totalGryffindor + gryffindor);
+		setSlytherin(totalSlytherin + slytherin);
+		setRavenclaw(totalRavenclaw + ravenclaw);
+		setHufflepuff(totalHufflepuff + hufflepuff);
+
+		switch (Math.max(totalGryffindor, totalSlytherin, totalRavenclaw, totalHufflepuff)) {
+			case totalGryffindor: setHouse("Gryffindor")
+				break;
+			case totalSlytherin: setHouse("Slytherin")
+				break;
+			case totalRavenclaw: setHouse("Ravenclaw")
+				break;
+			case totalHufflepuff: setHouse("Hufflepuff")
+				break;
+			default:
+				break; 
+		};
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowHouse(true);
+		}
+	};
+	
+	const [banner, setBanner] = useState('')
+	const changeBanner = () => {
+		if (house === 'Gryffindor' && showHouse===true) {
+			setBanner(require("./assets/banners/Gryffindor.jpg"));}
+		if (house === 'Slytherin' && showHouse===true) {
+			setBanner(require("./assets/banners/Slytherin.jpg"));}
+		if (house === 'Ravenclaw' && showHouse===true) {
+			setBanner(require("./assets/banners/Ravenclaw.jpg"));}
+		if (house === 'Hufflepuff' && showHouse===true) {
+			setBanner(require("./assets/banners/Hufflepuff.jpg"));}
+	};
+	// Always check which house has most points in order to reveal the respective banner
+	useEffect(() => {
+		changeBanner()
+	});
+
+	return (
+		<div>
+		<div className='main-title'>The Sorting Hat</div>
+		<div className='app' style={{borderRadius:'7px', backgroundPosition: '50%', backgroundBlendMode:'normal', backgroundImage: `url(${banner})`}}>
+			{showQuiz ? (
+			<div>
+			{showHouse ? (
+				<div className='score-section'>
+					You belong in 
+					<br/>
+					<p className='house'>{house}</p> 
+				</div>
+				
+			) : (
+				<>
+				<div className='part-two'>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].options.map((option) => (
+							<button onClick={() => {answerHandler(option.gryffindor, option.slytherin, option.ravenclaw, option.hufflepuff);}}>{option.optionText}</button>
+						))}
+					</div>
+				</div>
+					
+				</>
+			)} </div>
+		) : (
+		<>
+				<div className='intro-part'>
+					<div className='intro-text'>
+						Welcome to the Grand Hall! To find out which house you belong in, you'll be asked a series of questions about various topics, and you must answer as truthfully as you can. 
+						By the end, the hat will decide and tell you where you belong. Good luck!	
+					</div>
+					<button className='start-button button-loader' onClick={() => setShowQuiz(true)}>Start</button>
+				</div>
+		</>
+		)}
+		</div>
+		</div>
+	);
+}
---
+++ src/data/Questions.js
@@ -0,0 +1,93 @@
+export const questions = [
+    {
+        questionText: 'What class interests you the most?',
+        options: [
+            { optionText: 'Charms', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Potion making', gryffindor: 0, slytherin: 2, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Defence against dark arts', gryffindor: 0, slytherin: 0, ravenclaw: 2, hufflepuff: 0 },
+            { optionText: 'Transfiguration', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 2 },
+        ],
+    },
+    {
+        questionText: "If you were part of a team project, you would...",
+        options: [
+            { optionText: 'Do most of the work', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Only do what you know', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Do a bit of everything', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Rely on others', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'If you were an animal, what would you rather be?',
+        options: [
+            { optionText: 'Wolf', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Snake', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Horse', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Tiger', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'If you could take a luck potion to win a quidditch match, would you?',
+        options: [
+            { optionText: 'Absolutely', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Maybe', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'No', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'I don\'t play quidditch', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    
+    {
+        questionText: 'Among the following, Who is your favorite professor?',
+        options: [
+            { optionText: 'Severus Snape', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Minerva McGonagall', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Remus Lupin', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Alastor \'Mad-Eye\' Moody', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'In a 1 on 1 duel, what is your favorite spell?',
+        options: [
+            { optionText: 'Expelliarmus!', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Protego!', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Stupefy!', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Crucio!', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'How would you like to see yourself in the magic Mirror of Erised?',
+        options: [
+            { optionText: 'Rich beyond measure', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Highly esteemed', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Having wonderful adventures', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Surrounded with loved ones', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'If you could own a magic creature, which would you pick?',
+        options: [
+            { optionText: 'Unicorn', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Phoenix', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Dragon', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Chimera', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'What material would you choose for your wand?',
+        options: [
+            { optionText: 'Unicorn hair', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Phoenix feather', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Dragon heartstring', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Horned serpent horn', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+    {
+        questionText: 'Are you a potato?',
+        options: [
+            { optionText: 'Yes', gryffindor: 2, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'No', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'I\'m a carrot', gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 },
+            { optionText: 'Just show me where I belong', gryffindor: 0, slytherin: 2, ravenclaw: 0, hufflepuff: 0 },
+        ],
+    },
+];
